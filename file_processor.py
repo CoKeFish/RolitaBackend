e901d734dceb7927f6f08b6fd9b075cdf9c04d43
CoKeFish <rrtc26@gmail.com>
2024-09-03 02:46:18 -0500

    Bach de datos para aumentar la velocidad (con errores)

--- file_processor.py
+++ file_processor.py
@@ -1,8 +1,9 @@
 ﻿# file_processor.py
 import re
+import sqlite3
 
 from config import HEADERS_SPECIFIC, COMMON_HEADERS, versiones_trama_headers, conductores_headers, vehiculos_headers, \
-    inserted_vehiculos, inserted_versiones, inserted_conductores
+    inserted_vehiculos, inserted_versiones, inserted_conductores, foreign_keys
 from data_extractor import extract_json_objects, extract_values
 from database import create_connection, insert_data, create_tables
 
@@ -28,6 +29,15 @@ def process_file(input_path, output_path, db_path):
     # Cargar datos existentes al inicio
     load_existing_data(conn)
 
+    # Iniciar una transacción para inserciones por lotes
+    conn.execute('BEGIN')
+
+    # Contenedores para inserciones en lote
+    batch_vehiculos = []
+    batch_conductores = []
+    batch_versiones = []
+    batch_main = {tipo: [] for tipo in HEADERS_SPECIFIC.keys()}
+
     # Procesar cada objeto JSON extraído
     for obj in json_objects:
         # Identificar el tipo del objeto JSON usando las claves disponibles
@@ -45,31 +55,57 @@ def process_file(input_path, output_path, db_path):
 
             # Insertar datos en las tablas relacionadas antes de insertar en la tabla principal
             if vehiculos_data['idVehiculo'] and vehiculos_data['idVehiculo'] not in inserted_vehiculos:
-                # Insertar solo si el idVehiculo no está ya registrado
-                insert_data(conn, "Vehiculos", list(vehiculos_data.keys()), list(vehiculos_data.values()))
+                batch_vehiculos.append((list(vehiculos_data.values())))
                 inserted_vehiculos.add(vehiculos_data['idVehiculo'])  # Añadir a la lista de identificadores insertados
 
             if conductores_data['idConductor'] and conductores_data['idConductor'] not in inserted_conductores:
-                # Insertar solo si el idConductor no está ya registrado
-                insert_data(conn, "Conductores", list(conductores_data.keys()), list(conductores_data.values()))
+                batch_conductores.append((list(conductores_data.values())))
                 inserted_conductores.add(conductores_data['idConductor'])
 
             if versiones_trama_data['versionTrama'] and versiones_trama_data['versionTrama'] not in inserted_versiones:
-                # Insertar solo si la versionTrama no está ya registrada
-                insert_data(conn, "VersionesTrama", list(versiones_trama_data.keys()),
-                            list(versiones_trama_data.values()))
+                batch_versiones.append((list(versiones_trama_data.values())))
                 inserted_versiones.add(versiones_trama_data['versionTrama'])
 
             # Insertar los datos en la tabla principal correspondiente
-            insert_data(conn, tipo, list(main_data.keys()), list(main_data.values()))
-
+            batch_main[tipo].append(list(main_data.values()))
         else:
             # Proporciona un mensaje de depuración más detallado
             print(f"Tipo desconocido: {tipo} - Objeto JSON: {obj}")
 
+    # Ejecutar inserciones por lotes
+    if batch_vehiculos:
+        insert_data_batch(conn, "Vehiculos", vehiculos_headers, batch_vehiculos)
+    if batch_conductores:
+        insert_data_batch(conn, "Conductores", conductores_headers, batch_conductores)
+    if batch_versiones:
+        insert_data_batch(conn, "VersionesTrama", versiones_trama_headers, batch_versiones)
+
+    for tipo, batch in batch_main.items():
+        if batch:
+            insert_data_batch(conn, tipo, get_headers_for_type_names(tipo, COMMON_HEADERS, HEADERS_SPECIFIC, foreign_keys,vehiculos_headers, conductores_headers, versiones_trama_headers), batch)
+
+    # Confirmar la transacción en lote
+    conn.commit()
+
     # Cerrar la conexión a la base de datos
     if conn:
         conn.close()
+
+
+def insert_data_batch(conn, table_name, headers, data_batch):
+    """Inserta datos en una tabla específica en lote."""
+    placeholders = ', '.join('?' * len(headers))
+    columns = ', '.join(
+        [header.replace(' ', '_') for header, _ in headers])  # Asegúrate de que los nombres de columnas sean válidos
+
+    sql = f"INSERT OR REPLACE INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+    try:
+        cursor = conn.cursor()
+        cursor.executemany(sql, data_batch)
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error al insertar datos en la tabla {table_name}: {e} - SQL: {sql}")
 
 
 def prepare_data_for_insertion(obj, headers, vehiculos_headers, conductores_headers, versiones_trama_headers):
@@ -183,3 +219,38 @@ def load_existing_data(conn):
     # Cargar versiones de trama existentes
     cursor.execute("SELECT versionTrama FROM VersionesTrama")
     inserted_versiones = {row[0] for row in cursor.fetchall()}
+
+
+def get_headers_for_type_names(tipo, common_headers, specific_headers, foreign_keys, vehiculos_headers, conductores_headers, versiones_trama_headers):
+    """
+    Genera solo los nombres de los encabezados necesarios para la inserción de datos en la base de datos según el tipo de trama,
+    y los devuelve como tuplas de dos valores (nombre, tipo).
+
+    Args:
+    - tipo: El tipo de trama de datos.
+    - common_headers: Encabezados comunes para todas las tramas.
+    - specific_headers: Encabezados específicos para cada tipo de trama.
+    - foreign_keys: Claves foráneas que se deben agregar a los encabezados.
+    - vehiculos_headers: Encabezados de la tabla Vehiculos.
+    - conductores_headers: Encabezados de la tabla Conductores.
+    - versiones_trama_headers: Encabezados de la tabla VersionesTrama.
+
+    Returns:
+    - Lista de tuplas con dos valores: (nombre de encabezado, tipo).
+    """
+    # Excluir encabezados que pertenecen a otras tablas (Vehiculos, Conductores, VersionesTrama)
+    exclude_headers = {header for header, _ in vehiculos_headers + conductores_headers + versiones_trama_headers}
+
+    # Filtrar los nombres de los encabezados comunes excluyendo aquellos que están en el conjunto exclude_headers
+    filtered_common_headers = [(header, dtype) for header, dtype in common_headers if header not in exclude_headers]
+
+    # Añadir los encabezados específicos del tipo de trama
+    headers_for_type = filtered_common_headers + [(header, dtype) for header, dtype in specific_headers.get(tipo, [])]
+
+    # Añadir los nombres de columnas referenciadas en claves foráneas si no están ya presentes
+    for fk_column, _ in foreign_keys:
+        column_name = fk_column.split('(')[1].split(')')[0]  # Extraer el nombre de la columna
+        if column_name not in [header for header, _ in headers_for_type]:
+            headers_for_type.append((column_name, "INTEGER"))  # Añadir con un tipo por defecto (p.ej. INTEGER)
+
+    return headers_for_type  # Devolver la lista de tuplas (nombre, tipo)
